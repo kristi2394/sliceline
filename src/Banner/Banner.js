255bfe4932a6b685b2f937dcdf4982446b85ca77
KristiMihal1 <mihalkristi@yahoo.com>
2021-09-01 15:45:59 +0200

    banner styles

---
+++ src/Banner/Banner.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Banner = styled.div`
+  height: 400px;
+  background-image: url("img/wallpaper-banner.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,6 +6,8 @@ import { Title } from "../Styles/title";
 const NavbarStyled = styled.div`
   background-color: ${pizzaRed};
   padding: 15px;
+  position: fixed;
+  width: 100%;
 `;
 
 const Logo = styled(Title)`
