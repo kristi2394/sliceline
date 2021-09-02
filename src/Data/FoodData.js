ae62e405fedc6b4ba00b5978de03f8928bbe74b4
KristiMihal1 <mihalkristi@yahoo.com>
2021-09-02 12:21:41 +0200

    food section

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,38 +1,55 @@
-export const foods = [
+export const foodItem = [
   {
     name: "Cheese Pizza",
     img: "/img/pizza.jpg",
+    section: "Pizza",
   },
   {
     name: "Peperoni Pizza",
     img: "/img/pizza2.jpg",
+    section: "Pizza",
   },
   {
     name: "Burger",
     img: "/img/burger.jpg",
+    section: "Sandwitch",
   },
   {
     name: "Chicken Fingers",
     img: "/img/chicken-fingers.jpg",
+    section: "Sides",
   },
   {
     name: "Chicken Pizza",
     img: "/img/chicken-pizza.jpg",
+    section: "Pizza",
   },
   {
     name: "Fries",
     img: "/img/fries.jpg",
+    section: "Sides",
   },
   {
     name: "Healthy Pizza",
     img: "/img/healthy-pizza.jpg",
+    section: "Pizza",
   },
   {
     name: "Sandwitch",
     img: "/img/sandwitch.jpg",
+    section: "Sandwitch",
   },
   {
     name: "Gyro",
     img: "/img/gyro.jpg",
+    section: "Sandwitch",
   },
 ];
+
+export const foods = foodItem.reduce((res, food) => {
+  if (!res[food.section]) {
+    res[food.section] = [];
+  }
+  res[food.section].push(food);
+  return res;
+}, {});
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -7,18 +7,23 @@ const MenuStyled = styled.div`
   margin: 0px 400px 50px 20px;
   height: 1000px;
 `;
+
 export function Menu() {
   return (
     <MenuStyled>
-      <h1>Menu</h1>
-      <FoodGrid>
-        {foods.map((food) => (
-          <Food img={food.img}>
-            <FoodLabel />
-            {food.name}
-          </Food>
-        ))}
-      </FoodGrid>
+      {Object.entries(foods).map(([sectionName, foods]) => (
+        <>
+          <h1>{sectionName}</h1>
+          <FoodGrid>
+            {foods.map((food) => (
+              <Food img={food.img}>
+                <FoodLabel />
+                {food.name}
+              </Food>
+            ))}
+          </FoodGrid>
+        </>
+      ))}
     </MenuStyled>
   );
 }
